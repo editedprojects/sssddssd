6ad39883a7e2c0da91c9e6173f1159d3d5b25766
EdiTeD's Projects <42066039+editedprojects@users.noreply.github.com>
2018-11-10 08:36:10 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const moment = require("moment");
+const fs = require("fs");
+const sql = require("better-sqlite3");
+const client = new Discord.Client();
+const Codes = new Discord.Client();
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+const devs = ["455924214227009546"];
+const adminprefix = ["#"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'ssdsdddssddssdsdsddssdsddssdsddsdsdssddsdssddsdssetname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'skdslkdsljdsljsdjdsljds')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+ client.login(process.env.BOT_TOKEN); 
